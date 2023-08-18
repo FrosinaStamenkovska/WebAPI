43632ca2924defea05402129517ccc90a93c33d0
FrosinaStamenkovska <117694114+FrosinaStamenkovska@users.noreply.github.com>
2023-08-18 23:11:44 +0200

    Add files via upload

---
+++ Homework_Class02/Homework_Class02/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_Class02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAll()
+        {
+            var users = UserDb.Users;
+
+            return Ok(users);
+        }
+
+        [HttpGet("index/{index}")]
+        public ActionResult GetByIndex(int index)
+        {
+            try
+            {
+                if(index < 0)
+                {
+                    return BadRequest("The value for index cannot be negative number!");
+                }
+
+                if(index >= UserDb.Users.Count)
+                {
+                    return NotFound($"The element on index {index} does not exist!");
+                }
+
+                return Ok(UserDb.Users[index]);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured, try again later");
+            }
+            
+        }
+    }
+}
---
+++ Homework_Class02/Homework_Class02/UserDb.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework_Class02
+{
+    public static class UserDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "Frosina",
+            "Ana",
+            "Marko",
+            "Christos",
+            "Martin"
+
+        };
+    }
+}
